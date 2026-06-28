ba8a4b54bebbffa54981413d7350594c28914b13
Ritzsizl <ritzraha4u@gmail.com>
2026-06-28 11:40:29 +0530

    Redirecting changes

--- src/api/app.py
+++ src/api/app.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
 from src.api.routes import router
 
-
 app = FastAPI(
     title="FraudShield AI API",
-    description="Fraud detection API using ML, rule engine, and risk engine.",
+    description="Fraud detection API using ML, Rule Engine and Risk Engine.",
     version="1.0.0"
 )
 
 app.include_router(router)
+
+
+@app.get("/", include_in_schema=False)
+def home():
+    return RedirectResponse(url="/docs")
